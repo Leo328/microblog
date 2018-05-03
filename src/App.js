d82bdd4b870ee47c6f60c181ed8e1721ee52cd13
Leo Ng <leogetsthingsdone@gmail.com>
2018-05-03 15:40:34 -0700

    first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <div className="container">
+          <div className="row">
+            <div className="col">1 of 2</div>
+            <div className="col">2 of 2</div>
+          </div>
+        </div> */}
+      </div>
+    );
+  }
+}
+
+export default App;
