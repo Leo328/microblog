cfd93ae2e37a35ca93486740223a73d35216dff0
Leo Ng <leogetsthingsdone@gmail.com>
2018-05-08 19:15:10 -0700

    Tuesday

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            title: 'title placeholder',
+            body: 'body placeholder'
+        }
+    }
+
+    onTitleChange = (event) => {
+        this.setState({
+            title: event.target.value,
+            body: this.state.body
+        })
+    }
+
+    onBodyChange = (event) => {
+        this.setState({
+            title: this.state.title,
+            body: event.target.value
+        })
+    }
+
+    submitForm = (event) => {
+        console.log(this.state)
+        event.preventDefault()
+        this.setState({title: '', body: ''});
+    }   
+    
     render() {
         return (
             <div className="row">
-                <form action="submit the form">
+                <form onSubmit={this.submitForm}>
                     <div className="form-group">
-                        <input type="title" class="form-control"  placeholder="Title" />
+                        <input type="title" class="form-control"  placeholder="Title" value={this.state.title} onChange={this.onTitleChange}/>
                         <br/>
-                        <textarea className="form-control" id="exampleTextarea" placeholder="Body"rows="3"></textarea>
+                        <textarea className="form-control" id="exampleTextarea" placeholder="Body"rows="3" value={this.state.body} onChange={this.onBodyChange}></textarea>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
